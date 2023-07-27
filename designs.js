9a7e9aa9726d016863254518d1f0594ab1489236
Swarna Jang Maden <limbujungmaden@gmail.com>
2023-07-27 17:58:08 +0545

    only first grid color changed

--- designs.js
+++ designs.js
@@ -1,15 +1,33 @@
+function etchAsketch() {
+  console.log("HELLODARKNESS");
+  document.getElementById("draw").style.backgroundColor = "black";
+}
+
 function gridGenerator(e) {
+  console.log("hello world1");
   e.preventDefault();
   let column = document.createElement("div");
   let final = document.createElement("div");
   final.setAttribute("class", "container");
-
+  let edge = 10;
   let gridLocation = document.getElementById("gridHere");
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
+  for (let i = 0; i < edge; i++) {
+    for (let j = 0; j < edge; j++) {
       let box = document.createElement("div");
 
       box.setAttribute("class", "box");
+      box.setAttribute("id", "draw");
+
+      box.setAttribute(
+        `style`,
+        `width: calc(600px/${edge}); height: calc(600px/${edge})`
+      );
+      box.addEventListener("mouseover", etchAsketch);
+
+      // box.addEventListener("mouseover", (e) => {
+      //   etchAsketch;
+      // });
+
       column.appendChild(box);
     }
     final.appendChild(column);
